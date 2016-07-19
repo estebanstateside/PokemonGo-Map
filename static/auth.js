ff5a670666126036f8e0408ce49b9538c4757d6f
estebanstateside <esteban.a@wearestateside.com>
2016-07-19 17:27:25 -0600

    Login form integration v1

--- static/auth.js
+++ static/auth.js
@@ -1,1 +1,20 @@
-console.log("AUTH!");
+var allowedUsers = {
+    silverhaze : "12345",
+    papapaul : "12345",
+    xavi : "12345"
+};
+
+function validateFormOnSubmit(theForm) {
+    var username = theForm.username;
+    var password = theForm.password;
+
+    if (allowedUsers['username'] == password) {
+        $(".login-form-container").hide(300, function(){
+            $("#fullmap").show(500);
+        });
+    } else {
+        alert("Usuario y/o contraseña incorrectos, por favor intente denuevo!");
+    }
+
+    return false;
+}
