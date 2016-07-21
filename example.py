b57ae626d7ac6f0763be266f7ab525dfd32f8ab1
estebanstateside <esteban.a@wearestateside.com>
2016-07-20 21:03:53 -0600

    Fix for loginasdn

--- example.py
+++ example.py
@@ -907,8 +907,4 @@ def get_map():
 if __name__ == '__main__':
     args = get_args()
     register_background_thread(initial_registration=True)
-    app.run(
-        host=app.config.get("HOST", "www.mundo-pokemon.com"),
-        port=app.config.get("PORT", 80),
-        threaded=True
-    )
+    app.run(threaded=True, host=args.host, port=args.port)
